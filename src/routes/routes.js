55067e1c0e9f3c4ff30ead9215bb5ae21d57e077
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-01-30 18:49:36 -0300

    Made the tasks 8 e 9

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginController = require('../controllers/login.controller');
 const { createUser, getAllUsers, getUserById } = require('../controllers/user.controller');
+const { insertCategory, getAllCategories } = require('../controllers/category.controller');
 const authUser = require('../middlewares/authUser');
 
 const router = express.Router();
@@ -14,5 +15,7 @@ router.use(authUser);
 
 router.get('/user', getAllUsers);
 router.get('/user/:id', getUserById);
+router.post('/categories', insertCategory);
+router.get('/categories', getAllCategories);
 
 module.exports = router;
